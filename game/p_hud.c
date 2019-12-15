a382aa3b967810aa2cec49af75af916105e62eef
Akhil Samarth <akhilsamarth236@gmail.com>
2019-12-14 21:02:39 -0500

    Changed help menu into black background.

--- game/p_hud.c
+++ game/p_hud.c
@@ -299,36 +299,14 @@ HelpComputer
 Draw help computer.
 ==================
 */
+// modified to draw instructions for class switching
 void HelpComputer (edict_t *ent)
 {
 	char	string[1024];
-	char	*sk;
-
-	if (skill->value == 0)
-		sk = "easy";
-	else if (skill->value == 1)
-		sk = "medium";
-	else if (skill->value == 2)
-		sk = "hard";
-	else
-		sk = "hard+";
 
 	// send the layout
-	Com_sprintf (string, sizeof(string),
-		"xv 32 yv 8 picn help "			// background
-		"xv 202 yv 12 string2 \"%s\" "		// skill
-		"xv 0 yv 24 cstring2 \"%s\" "		// level name
-		"xv 0 yv 54 cstring2 \"%s\" "		// help 1
-		"xv 0 yv 110 cstring2 \"%s\" "		// help 2
-		"xv 50 yv 164 string2 \" kills     goals    secrets\" "
-		"xv 50 yv 172 string2 \"%3i/%3i     %i/%i       %i/%i\" ", 
-		sk,
-		level.level_name,
-		game.helpmessage1,
-		game.helpmessage2,
-		level.killed_monsters, level.total_monsters, 
-		level.found_goals, level.total_goals,
-		level.found_secrets, level.total_secrets);
+	Com_sprintf(string, sizeof(string),
+		"xv -140 yv -80 picn blackbg ");
 
 	gi.WriteByte (svc_layout);
 	gi.WriteString (string);
